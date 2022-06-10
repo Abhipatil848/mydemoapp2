c353ab4c4ac56b7081b3d27d4754daeed2b2f51d
Abhishek <abhishekpatil5510@gmail.com>
2022-06-10 12:44:32 +0530

    refactor: react bootstrap added and navabar as component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Navigation from './components/Navigation/navigation';
+
 function App() {
   return (
     <div className="App">
+      <Navigation />
       <header className="App-header">
+
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          
         </p>
         <a
           className="App-link"
@@ -18,6 +23,7 @@ function App() {
           Learn React
         </a>
       </header>
+      
     </div>
   );
 }
